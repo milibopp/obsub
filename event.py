2472e1e4a32ef519c414c9fb6363c6e30c291ce6
Thomas Gläßle <t_glaessle@gmx.de>
2013-08-14 04:56:37 +0200

    Call the decorated function before the event handlers
    - allows to prepare a expectable state for all handlers
    - enforces a common calling signature for the event

--- event.py
+++ event.py
@@ -26,18 +26,18 @@ class event(object):
     ...         self.name = name
     ...
     ...     @event
-    ...     def progress(self):
-    ...         pass
-    ...
-    ...     def do_something(self):
+    ...     def progress(self, first, second):
     ...         print("Doing something...")
-    ...         self.progress()
 
-    A.progress is the event.  It is triggered within the method A.do_something.
+    A.progress is the event.  It is triggered after executing the code in the
+    decorated progress routine.
     Now that we have a class with some event, let's create an event handler.
 
-    >>> def handler(self):
-    ...     print("Hello %s!" % self.name)
+    >>> def handler(self, first, second):
+    ...     print("%s %s and %s!" % (first, self.name, second))
+
+    Note that the handler (and signal calls) must have the signature defined
+    by the decorated event method.
 
     This handler only greets the object that triggered the event by using its
     name attribute.  Let's create some instances of A and register our new
@@ -51,12 +51,19 @@ class event(object):
     Now everything has been setup.  When we call the method, the event will be
     triggered:
 
-    >>> a.do_something()
+    >>> a.progress("Hello", "World")
     Doing something...
-    Hello Foo!
-    >>> b.do_something()
+    Hello Foo and World!
+    >>> b.progress(second="Others", first="Hi")
     Doing something...
-    Hello Bar!
+    Hi Bar and Others!
+
+    What happens if we disobey the call signature?
+
+    >>> c = A("World")
+    >>> c.progress(second="World")  # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    TypeError: progress() missing 1 required positional argument: 'first'
 
     '''
 
@@ -71,6 +78,9 @@ class event(object):
         self.__doc__ = function.__doc__
         # Use its name as key
         self._key = ' ' + function.__name__
+        # Used to enforce call signature even when no slot is connected.
+        # Can also execute code (called before handlers)
+        self._function = function
 
     def __get__(self, instance, owner):
         '''
@@ -91,7 +101,7 @@ class event(object):
             # On the first try this raises a KeyError,
             # The error is caught to write the new entry into the instance dictionary.
             # The new entry is an instance of boundevent, which exhibits the event behaviour.
-            be = instance.__dict__[self._key] = boundevent(instance)
+            be = instance.__dict__[self._key] = boundevent(instance, self._function)
             return be
 
 
@@ -100,7 +110,7 @@ class boundevent(object):
     Private helper class for event system.
     '''
 
-    def __init__(self, instance):
+    def __init__(self, instance, function):
         '''
         Constructor.
 
@@ -110,6 +120,7 @@ class boundevent(object):
         # Create empty list of registered event handlers.
         self.__event_handlers = []
         self.__instance = weakref.ref(instance)
+        self.__function = function
 
     @property
     def instance(self):
@@ -154,7 +165,12 @@ class boundevent(object):
         # Create a hard ref to the instance to make sure it survives
         # througout this function call
         instance = self.instance
+        # Enforce signature and possibly execute entry code. This makes sure
+        # any inconsistent call will be caught immediately, independent of
+        # connected handlers.
+        result = self.__function(instance, *args, **kwargs)
         # Call all registered event handlers
         for f in self.__event_handlers[:]:
             f(instance, *args, **kwargs)
+        return result
 
