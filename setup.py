76566967b61396ba0d7f73546ed6a9344c85116f
Thomas Gläßle <t_glaessle@gmx.de>
2013-09-17 00:44:30 +0200

    Convert markdown to reST for PyPI in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from distutils.spawn import find_executable
+
+# Convert markdown to reST for PyPI:
+pandoc = find_executable('pandoc')
+if pandoc:
+    from subprocess import check_output
+    long_description = check_output(
+            [pandoc, 'README.md', '-t', 'rst'],
+            universal_newlines=True)
+else:
+    long_description = None
+
 
 setup(name='obsub',
     version='0.1.1',
     description='Implementation of the observer pattern via a decorator',
+    long_description=long_description,
     author='Emilia Bopp',
     author_email='Emilia.bopp@aepsil0n.de',
     url='https://github.com/aepsil0n/obsub',
