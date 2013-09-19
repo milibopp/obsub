79a615e6098627855fe3464d5e0331e0d4a8f720
Thomas Gläßle <t_glaessle@gmx.de>
2013-09-20 00:49:36 +0200

    Delete the obvious hard-ref to the object before invoking gc

--- test/test_weakref.py
+++ test/test_weakref.py
@@ -63,6 +63,7 @@ def test_object_stays_alive_during_handler_execution():
             # delete the A-instance
             del self.a
             a.deleted = True
+            del a
             gc.collect()
 
     def handler(a):
