2bb5a4dac29898d5c372b0d6a9d883eb542facb1
Thomas Gläßle <t_glaessle@gmx.de>
2013-09-21 20:33:25 +0200

    Fix old-style classes [HOT]

--- obsub.py
+++ obsub.py
@@ -150,7 +150,7 @@ class boundevent(object):
         '''
         self.instance = instance
         self.__function = function
-        self.__key = function.__name__
+        self.__key = ' ' + function.__name__
 
     @property
     def __event_handlers(self):
