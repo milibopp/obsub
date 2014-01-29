839b96f297133404d39bf6e28c9a62ffc16ce9b0
Emilia Bopp <contact@ebopp.de>
2014-01-29 13:17:12 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ f.close()
 
 setup(
     name='obsub',
-    version='0.1.1',
+    version='0.2',
     description='Implementation of the observer pattern via a decorator',
     long_description=long_description,
     author='Emilia Bopp',
